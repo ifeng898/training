e30bf9c7b084b3d9a12cd8b4dac176df44a59dad
jysw8 <ifeng898@gmail.com>
2022-09-04 22:57:04 -0700

    version05 新增技术 配置项目内容正确性检查

--- src/main/java/com/example/demo/model/Child.java
+++ src/main/java/com/example/demo/model/Child.java
@@ -2,9 +2,14 @@ package com.example.demo.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class Child {
+    @NotEmpty
     private String name;
+    @Min(1)
     private int age;
     private Friend[] friends;
 }
--- src/main/java/com/example/demo/model/Father.java
+++ src/main/java/com/example/demo/model/Father.java
@@ -2,8 +2,11 @@ package com.example.demo.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class Father {
+    @NotEmpty
     private String name;
     private int age;
 }
